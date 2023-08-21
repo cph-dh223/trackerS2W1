429d2982e8ad6f30576d5f4b7a927ba90a578a74
DavidAH <cph-dh223@cphbusiness.dk>
2023-08-21 12:07:10 +0200

    ubdated gitignore

---
+++ tests/ReadFile.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ReadFile {
+    @Test
+    public void getAll(){
+        
+        // assertEquals(1, 1);
+    }
+}
